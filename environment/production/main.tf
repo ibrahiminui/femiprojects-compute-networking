11d1ac3954be2cafc01cf83eac6716b1119dfa79
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-02 17:54:03 -0500

    delete network

--- environment/production/main.tf
+++ environment/production/main.tf
@@ -1,10 +1,12 @@
+
+/*
 resource "google_compute_network" "this" {
   name    = var.network_name
   project = var.project_id
 
   auto_create_subnetworks = false
 }
-
+*/
 /*
 
 resource "google_compute_subnetwork" "this" {
