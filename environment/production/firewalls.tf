d641fbd81aa3277309cc0ec0141365c76681dac0
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-02 23:02:23 -0500

    adding network

--- environment/production/firewalls.tf
+++ environment/production/firewalls.tf
@@ -2,14 +2,13 @@ resource "google_compute_firewall" "allow-ssh" {
   name    = "allow-ssh-access"
   network = google_compute_network.compute_network.name
 
-  source_ranges =["0.0.0.0/0"]
+  source_ranges = ["0.0.0.0/0"]
 
   allow {
     protocol = "tcp"
     ports    = ["80"]
   }
 
-
-  direction = "INGRESS"
+  direction   = "INGRESS"
   source_tags = ["web"]
 }
