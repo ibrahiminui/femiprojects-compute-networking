25208e2785f7a15f56479b8f6c438651ed82aa32
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-02 23:09:38 -0500

    adding network

--- environment/production/firewalls.tf
+++ environment/production/firewalls.tf
@@ -7,9 +7,39 @@ resource "google_compute_firewall" "allow-ssh" {
 
   allow {
     protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  direction   = "INGRESS"
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "allow-http-traffic"
+  network = google_compute_network.compute_network.name
+  project = var.project_id
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
     ports    = ["80"]
   }
 
   direction   = "INGRESS"
-  source_tags = ["web"]
+}
+
+resource "google_compute_firewall" "google-healthcheck" {
+  name    = "allow-google-healthcheck"
+  network = google_compute_network.compute_network.name
+  project = var.project_id
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = []
+  }
+
+  direction   = "INGRESS"
+
 }
