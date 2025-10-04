5ddf15cb72f7ff0992e02eee191bd676ec36389c
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-03 19:14:58 -0500

    creating network resources

--- environment/production/shared-vpc-config.tf
+++ environment/production/shared-vpc-config.tf
@@ -5,7 +5,7 @@ resource "google_compute_shared_vpc_host_project" "shared-vpc-host" {
 
 # A service project gains access to network resources provided by its
 # associated host project.
-resource "google_compute_shared_vpc_service_project" "shared-service-" {
+resource "google_compute_shared_vpc_service_project" "shared-service" {
   host_project    = google_compute_shared_vpc_host_project.shared-vpc-host.project
   service_project = "global-web-server-473500"
 }
