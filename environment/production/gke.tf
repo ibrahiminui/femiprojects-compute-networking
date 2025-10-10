052845b0c74bc27d78137456ba4dfe88d32bdc0c
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-09 20:25:49 -0500

    adding module

--- environment/production/gke.tf
+++ environment/production/gke.tf
@@ -1,15 +1,15 @@
 
 module "us-west2-gke-cluster" {
 
-source = "../../modules/gke-cluster"
-region = "us-west2"
+  source = "../../modules/gke-cluster"
+  region = "us-west2"
 
 }
 
 
 resource "google_container_node_pool" "us-west2-gke-node-pool" {
-  name       = "us-west2-gke-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.us-west2-gke-cluster.name
+  name     = "us-west2-gke-node-pool"
+  location = var.region
+  cluster  = google_container_cluster.us-west2-gke-cluster.name
 
 }
