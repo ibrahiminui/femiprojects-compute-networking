3b3481de00a8e01ab7dfcb1fe0bbfc80636a61af
Femi Ibrahim <femiibrahim@Mac.lan>
2025-10-11 17:59:11 -0500

    update project

--- environment/production/gke.tf
+++ environment/production/gke.tf
@@ -1,6 +1,5 @@
 
 module "us-west2-gke-cluster" {
-
   source                = "../../modules/gke-cluster"
   gke-project           = "global-web-server-473500"
   region                = "us-west2"
@@ -8,13 +7,9 @@ module "us-west2-gke-cluster" {
   shared-vpc-network    = "projects/global-shared-networking/global/networks/compute-us-west2-network"
 }
 
-
-
-
 resource "google_container_node_pool" "us-west2-gke-node-pool" {
   name     = "us-west2-gke-node-pool"
   project  = "global-web-server-473500"
   location = var.region
   cluster  = module.us-west2-gke-cluster.gke-cluster-name
-
 }
