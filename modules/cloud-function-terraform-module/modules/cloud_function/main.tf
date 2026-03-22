251d1281429443699d80f6bbd06059ca1140264d
ibrahiminui <ibrahiminui@gmail.com>
2026-03-22 18:32:41 -0500

    deploy cloud functions

--- modules/cloud-function-terraform-module/modules/cloud_function/main.tf
+++ modules/cloud-function-terraform-module/modules/cloud_function/main.tf
@@ -90,10 +90,10 @@ resource "google_cloudfunctions2_function" "function" {
   ]
 }
 
-resource "google_cloudfunctions2_function_iam_member" "invoker" {
-  project        = var.project_id
-  location       = var.region
-  cloud_function = google_cloudfunctions2_function.function.name
-  role           = "roles/run.invoker"
-  member         = var.invoker_member
+resource "google_cloud_run_v2_service_iam_member" "invoker" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloudfunctions2_function.function.name
+  role     = "roles/run.invoker"
+  member   = var.invoker_member
 }
